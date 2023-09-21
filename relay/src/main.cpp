0f744b09223d23cdff1d8e5d5831982c23a25600
Doszhan <doszhan.m@mail.ru>
2023-09-21 20:49:13 +0600

    init:relay

--- relay/src/main.cpp
+++ relay/src/main.cpp
@@ -1,43 +1,12 @@
 #include <Arduino.h>
-#include <RCSwitch.h>
 
-
-// ----------------------- ПИНЫ --------------------------------------------------------------------
-#define RADIO_VCC 12                                         // питание радио модуля
-#define RADIO_GND 9                                        // GND радио модуля
-#define RADIO_DATA 11                                       // data пин радио модуля
-
-
-// Классы ------------------------------------------------------------------------------------------
-RCSwitch radioModule = RCSwitch();
-
+// put function declarations here:
 
 void setup() {
-    Serial.begin(9600);
-
-    pinMode(RADIO_VCC, OUTPUT);           
-    digitalWrite(RADIO_VCC, HIGH);       
-    pinMode(RADIO_GND, OUTPUT);         
-    digitalWrite(RADIO_GND, LOW);
-
-    radioModule.enableReceive(11);
+  Serial.begin(115200);
 }
 
-
 void loop() {
-    Serial.println("Check Arduino Receive");
-    Serial.println(radioModule.available());
-    Serial.println(radioModule.getReceivedValue());
-
-    if( radioModule.available() ){
-        int value = radioModule.getReceivedValue();
- 
-        if( value == B1000 )
-            Serial.println("Receive 1");
-        else if( value == B0100 )
-            Serial.println("Receive 2");
-
-        radioModule.resetAvailable();
-    }
-    // delay(1000); 
+  Serial.println("Initializing");
+  delay(1000);
 }
